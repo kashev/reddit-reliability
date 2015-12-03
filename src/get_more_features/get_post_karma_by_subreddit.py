84769899927bebc31ae72327a52afb4dedf8a776
Kashev Dalmia <kashev.dalmia@gmail.com>
2015-12-03 17:23:41 -0600

    adding get post karma by subreddit

---
+++ src/get_more_features/get_post_karma_by_subreddit.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+## -*- coding: utf-8 -*-
+# reddit-reliability
+
+from collections import defaultdict
+import pymongo
+import pprint
+from pymongo import MongoClient
+
+
+def main():
+    mongo_client = MongoClient('mongodb://cs598tar:cs598tar@'
+                               '107.170.215.176:27017')
+    reddit_data = mongo_client.reddit_data
+    print(reddit_data.collection_names(include_system_collections=False))
+    user_data = reddit_data.user_data
+    user_submitted = reddit_data.user_submitted
+    post_karma_by_subreddit = reddit_data.post_karma_by_subreddit
+
+    post_karma_by_subreddit.create_index(
+        [("username", pymongo.ASCENDING)],
+        background=True,
+        unique=True,
+        dropDups=True
+    )
+
+    for user_mongo in user_data.find():
+        user_name = user_mongo['data']['name']
+        print(user_name)
+
+        karma_by_subreddit = defaultdict(int)
+        for submission in user_submitted.find({"data.author": user_name}):
+            subreddit = submission['data']['subreddit'].replace('.', '')
+            score = submission['data']['score']
+            karma_by_subreddit[subreddit] += score
+        try:
+            post_karma_by_subreddit.insert_one({
+                "username": user_name,
+                "karma_by_subreddit": dict(karma_by_subreddit)
+            })
+        except pymongo.errors.DuplicateKeyError:
+            continue
+
+
+if __name__ == '__main__':
+    main()
